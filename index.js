3071edbb5d648c1f935a1cc571b9e34f56fb2e2a
Glen Pike <glen.pike@headforwards.com>
2016-02-02 14:13:43 +0000

    glen-socket-2-way: Flesh out getting list of id's and using.
    Make socket request a list of vehicle id's from the server,
    which it uses to populate the select boxes.
    Change commands to look more like the ones in the API.

--- index.js
+++ index.js
@@ -9,11 +9,31 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+function handleMessage(msg) {
+
+    var response = {};
+
+    var command = msg.command;
+
+    switch(command) {
+        case 'list_vehicle_ids':
+            response = ['red', 'green', 'blue'];
+            break;
+        default:
+            response = {
+                command: command,
+                vehicle_id: msg.vehicle_id
+            }
+    }
+
+    io.emit('message', response);
+}
+
 io.on('connection', function(socket){
-  socket.on('message', function(msg){
-    io.emit('message', msg);
-  });
+  socket.on('message', handleMessage);
 });
+
+
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
--- js/main.js
+++ js/main.js
@@ -4,18 +4,52 @@
 var socket = io();
 
 socket.on('message', function(msg){
-    var myCar = $('#myCar').val();
-
-    if (msg.car === myCar || myCar === 'All') {
-        // FIXME inline styles, just for demo!
-        $('#messages').append($('<li style="color:' + msg.car + '">').text(msg.action));
+    if(msg.command) {
+        handleCommand(msg);
+    } else {
+        handleVehicleIds(msg);
     }
 });
 
+function handleVehicleIds(msg) {
+    // <option value="red">Red Car</option>
+    //
+    if(!msg || !msg.length) {
+        console.log('handleVehicleIds no ids!');
+    }
+
+    var sourceCar = document.querySelector('#selCar'),
+        targetCar = document.querySelector('#myCar');
+
+    msg.forEach(function(id) {
+        var option = document.createElement('option');
+
+        option.value = id;
+        option.appendChild(document.createTextNode(id + ' car'));
+
+        sourceCar.appendChild(option);
+
+        targetCar.appendChild(option.cloneNode(true));
+    })
+}
+
+function handleCommand(msg) {
+    var myCar = $('#myCar').val();
+
+
+    if (msg.vehicle_id === myCar || myCar === 'All') {
+        // FIXME inline styles, just for demo!
+        $('#messages').append($('<li style="color:' + msg.vehicle_id + '">').text(msg.command));
+    }
+}
+
 function emitMessage(message) {
     var msg = {
-        car: $('#selCar').val(),
-        action: message
+        vehicle_id: $('#selCar').val(),
+        command: message
     };
     socket.emit('message', msg);
 }
+
+
+socket.emit('message', { command: 'list_vehicle_ids'} );
