e920c11ebba2f233bf8168c9e069a050c985dd2e
Glen Pike <glen.pike@headforwards.com>
2016-02-23 14:17:57 +0000

    glen-socket-2-way: Socket and car object.
    Create simple "car" prototype object.
    Start fleshing out socket handling better (needs encapsulation)

--- js/main.js
+++ js/main.js
@@ -1,46 +1,69 @@
 /**
  * Created by Rich on 30-Jan-16.
  */
-var socket = io('http://localhost:8000');
 
-socket.on('message', function(msg){
-    if(msg.error) {
-        console.log('error ', msg)
 
-    } else if(msg.command) {
-        handleCommand(msg);
-    } else {
-        handleVehicleIds(msg);
+/* Very simplistic "car" object which draws itself and handles messages / commands
+*/
+var carProto = {
+    _vehicle_id: null,
+    $car: null,
+    init: function($container, vehicle_id) {
+        this._vehicle_id = vehicle_id;
+        //Render itself
+        this._render()
+        $container.append(this.$car);
+    },
+
+    _render: function() {
+        this.$car = $('<div class="car" id="' + this._vehicle_id + '"/>');
+
+        this.$car.html('<ul class="messages"/>');
+    },
+
+    _renderMessage: function(message) {
+        $('messages', this.$car).append(
+            $('<li class="message">').text(message));
+    },
+
+    handleCommand: function(msg) {
+        this._renderMessage(msg.command);
     }
-});
+};
+
+var socket = io('http://localhost:8000');
 
 socket.on('json', function(msg){
     console.log('json ', msg)
-    if(msg.command) {
-        handleCommand(msg);
+    if(msg.error) {
+        console.log('error ', msg)
+
+    } else if(!msg.command) {
+        console.log('error - no command', msg)
+    } else if(msg.command === 'list_vehicle_ids') {
+        handleVehicleIds(msg);
+    } else {
+        ///
     }
 });
 
+var cars = {};
+
 function handleVehicleIds(msg) {
-    // <option value="red">Red Car</option>
-    //
-    if(!msg || !msg.length) {
-        console.log('handleVehicleIds no ids!');
+    try {
+        var $container = $('#car-container');
+        $container.html();
+
+        msg.response.vehicle_ids.forEach(function(id) {
+            var car = Object.create(carProto);
+            cars[id] = car;
+            car.init($container, id);
+        });
+
+
+    } catch(e) {
+        console.error('handleVehicleIds message error ', msg, e);
     }
-
-    var sourceCar = document.querySelector('#selCar'),
-        targetCar = document.querySelector('#myCar');
-
-    msg.forEach(function(id) {
-        var option = document.createElement('option');
-
-        option.value = id;
-        option.appendChild(document.createTextNode(id + ' car'));
-
-        sourceCar.appendChild(option);
-
-        targetCar.appendChild(option.cloneNode(true));
-    })
 }
 
 function handleCommand(msg) {
@@ -61,6 +84,24 @@ function emitMessage(message) {
     socket.emit('json', msg);
 }
 
+function parseSearch() {
+    var search = {};
+    document.location.search.replace('?', '')
+        .split('&')
+        .forEach(function(search) {
+            var arr = search.split('='), obj = {};
+            search[arr[0]] = arr[1];
+        });
+    return search;
+}
+
 socket.on('connect', function() {
-    socket.emit('json', { command: 'list_vehicle_ids'} );
+    console.log('connected!')
+    var command = { command: 'list_vehicle_ids' };
+    search = parseSearch();
+    if(search.email) {
+        command.email = search.email;
+    }
+
+    socket.emit('json', command);
 });
