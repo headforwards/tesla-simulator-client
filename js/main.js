610186928d62fbe8326230a4c236659f969e794f
Rich Frost <richard@ipponsolutions.co.uk>
2016-03-06 23:15:31 +0000

    Added car visualisation and flash lights on it when command received

--- js/main.js
+++ js/main.js
@@ -6,6 +6,21 @@ var socket = io();
 socket.on('message', function(msg){
     var myCar = $('#myCar').val();
 
+    if (msg.action.indexOf('flash_lights') > -1) {
+
+        var leftHeadlight = $('.js-left-headlight'),
+            rightHeadlight = $('.js-right-headlight');
+
+        leftHeadlight.addClass('on').delay(750).queue(function(next){
+            $(this).removeClass("on");
+            next();
+        });
+        rightHeadlight.addClass('on').delay(750).queue(function(next){
+            $(this).removeClass("on");
+            next();
+        });
+
+    }
     if (msg.car === myCar || myCar === 'All') {
         // FIXME inline styles, just for demo!
         $('#messages').append($('<li style="color:' + msg.car + '">').text(msg.action));
